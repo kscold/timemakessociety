b375f02f422f8bd9e377f6192fb54742716260a9
kscold <chan6502@gmail.com>
2024-05-16 20:18:08 +0900

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -153,13 +153,13 @@ export default defineConfig({
     //     },
     //   }),
   ],
-  // server: {
-  //   proxy: {
-  //     "/api": {
-  //       target: "http://kscoldproject.site",
-  //       changeOrigin: true,
-  //       rewrite: (path) => path.replace(/^\/api/, "/api"),
-  //     },
-  //   },
-  // },
+  server: {
+    proxy: {
+      "/api": {
+        target: "https://kscoldproject.site", // HTTPS로 변경
+        changeOrigin: true,
+        rewrite: (path) => path.replace(/^\/api/, "/api"),
+      },
+    },
+  },
 })
